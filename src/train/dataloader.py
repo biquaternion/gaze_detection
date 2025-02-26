68478814b8485bdfeb82f6c222fcf0dfe4f1317a
dmitry <biquaternion@gmail.com>
2025-02-26 23:33:42 +0400

    Add the routine of making black patches

--- src/train/dataloader.py
+++ src/train/dataloader.py
@@ -4,6 +4,7 @@
 import json
 from os import PathLike
 from pathlib import Path
+from typing import Tuple
 
 import numpy as np
 import pandas as pd
@@ -11,9 +12,10 @@ import torch
 from PIL import Image
 import cv2
 
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from torchvision.transforms import transforms
-from torchvision.transforms.functional import pil_to_tensor
+
+from src.utils.image_utils import add_black_paddings
 
 MIN_WIDTH = 60
 MAX_WIDTH = 224
@@ -25,10 +27,14 @@ class GazeTDataset(Dataset):
     def __init__(self,
                  data_path: PathLike = PROCESSED_GAZET_PATH,
                  split: str = 'test',
+                 fit_size_policy: str = 'resize',
+                 target_size: Tuple[int, int] = (448, 224),
                  transform=None):
         assert split in ['train', 'test'], 'split must be "train" or "test"'
         self.data_path = Path(data_path)
         self.split = split
+        self.fit_size_policy = fit_size_policy
+        self.target_size = target_size
         self.transform = transform
 
         self.images_path = self.data_path / 'images'
@@ -45,13 +51,17 @@ class GazeTDataset(Dataset):
         task_id = md['task_id']
         step = md['step']
         image_path = self.images_path / f'{task_id}' / f'{step}'
-        image = Image.open(image_path).convert('RGB')
+        image = cv2.imread(str(image_path), cv2.IMREAD_COLOR)
         relative_x, relative_y = md['relative_x'], md['relative_y']
         screen_x, screen_y = md['screen_size_x'], md['screen_size_y']
 
+        if self.fit_size_policy == 'resize':
+            image = cv2.resize(image, dsize=self.target_size)
+        elif self.fit_size_policy == 'padding':
+            image = add_black_paddings(src=image, target_ratio_wh=(self.target_size[0], self.target_size[1]))
         targets = [relative_x * screen_x, relative_y * screen_y]
         if self.transform:
-            image, targets = self.transform(image), torch.tensor(targets).float()
+            image, targets = self.transform(Image.fromarray(image)), torch.tensor(targets).float()
         return image, targets
 
 
--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -5,6 +5,7 @@ import logging
 from pathlib import Path
 from typing import Tuple, Union, Iterable
 
+import PIL.Image
 import cv2
 import numpy as np
 
@@ -35,11 +36,26 @@ def get_img_roi(src, inner_points: Iterable[Tuple], logger: logging.Logger = log
     return l.item(), t.item(), r.item(), b.item()
 
 
+def add_black_paddings(src: Union[np.ndarray, PIL.Image.Image], target_ratio_wh: Tuple[int, int] = (448, 224)) -> np.ndarray:
+    h, w = src.shape[:2]
+    tw, th = target_ratio_wh
+    if w / h > tw / th:
+        new_h = w * th // tw
+        new_w = w
+    else:
+        new_w = h * tw // th
+        new_h = h
+    result = np.zeros((new_h, new_w, 3), np.uint8)
+    result[(new_h - h) // 2:(new_h + h) // 2, (new_w - w) // 2:(new_w + w) // 2] += src
+    return result
+
+
 if __name__ == '__main__':
     images_path = Path('data/GazeT/dataset/images')
     images_paths = [('9492833e-ac73-467d-85c5-5b0dbc09f669', 'step_9.jpeg', [[522, 366], [673, 376], [734, 376],[589, 371]]),
                     ('9492833e-ac73-467d-85c5-5b0dbc09f669', 'step_14.jpeg', [[525, 391], [678, 398], [742, 393], [593, 396]]),
-                    ('8f0ba296-96d9-4ff0-8de9-13cc0e2ae671', 'step_1.jpeg', [(543, 698)])]
+                    ('8f0ba296-96d9-4ff0-8de9-13cc0e2ae671', 'step_1.jpeg', [(543, 698)])] + \
+                    [('5db1dd3e-cdb1-4b54-9422-c57e6b60e9dc', f'step_{i}.jpeg', [[1044, 609], [1006, 614], [958, 616], [919, 613]]) for i in range(1, 43)]
     for task, step, inner_points in images_paths:
         image_path = images_path / task / step
         image = cv2.imread(str(image_path))
@@ -48,7 +64,9 @@ if __name__ == '__main__':
         if roi is not None:
             l, t, r, b = roi
             cv2.imshow('image', image[t:b, l:r])
+            pad = add_black_paddings(image[t:b, l:r], target_ratio_wh=(2, 1))
+            print(l, r, t, b, (r - l) / (b - t), pad.shape[1] / pad.shape[0])
+            cv2.imshow('pad', pad)
             cv2.waitKey()
-            print(l, r, t, b)
         else:
             logger.warning(f'no roi for {image_path}')
